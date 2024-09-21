5a7c60b6351cd5fa6528dc5df73fa20d7350acd7
Tan Yi Jun <tanyijun6@gmail.com>
2024-09-21 12:49:03 +0800

    Add 404 page

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
+const page404 = fs.readFileSync('404.html', (err, data) => {
+  if (err) throw err;
+  return data;
+});
+
 http.createServer((req, res) => {
   const q = url.parse(req.url, true);
-  const filename = q.pathname === '/' ? 'index.html' : '.' + q.pathname;
+  const filename = q.pathname === '/' ? 'index.html' : '.' + q.pathname + '.html';
   fs.readFile(filename, (err, data) => {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
+      res.write(page404);
       return res.end();
     }
     res.writeHead(200, {'Content-Type': 'text/html'});
